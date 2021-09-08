f2281ef28cbdf0ad221eb034e05d21e9b1016452
Joakim <jodi@itu.dk>
2021-09-08 15:51:11 +0200

    Created user interface for IsLeapYear.

--- BDSA21.Tests/UnitTest1.cs
+++ BDSA21.Tests/UnitTest1.cs
@@ -11,15 +11,40 @@ namespace BDSA21.Tests
         {
             Program testProgram = new Program();
 
-            var actual1 = testProgram.calculateLeapYear(1800);
-            var actual2 = testProgram.calculateLeapYear(1900);
-            var actual3 = testProgram.calculateLeapYear(1600);
-            var actual4 = testProgram.calculateLeapYear(2004);
+            var actual1 = testProgram.IsLeapYear(1800);
+            var actual2 = testProgram.IsLeapYear(1900);
+            var actual3 = testProgram.IsLeapYear(1600);
+            var actual4 = testProgram.IsLeapYear(2004);
 
             Assert.False(actual1);
             Assert.False(actual2);
             Assert.True(actual3);
             Assert.True(actual4);
         }
+
+        
     }
+
+    public class UnitTest2
+    {
+        [Fact]
+        public void Check_if_calculateLeapYear_takes_user_input()
+        {
+            Program testProgram = new Program();
+
+            var actual1 = testProgram.calculateLeapYear("1800");
+            var actual2 = testProgram.calculateLeapYear("1900");
+            var actual3 = testProgram.calculateLeapYear("1600");
+            var actual4 = testProgram.calculateLeapYear("2004");
+
+            Assert.Equal(actual1, "nay");
+            Assert.Equal(actual2, "nay");
+            Assert.Equal(actual3, "yay");
+            Assert.Equal(actual4, "yay");
+        }
+
+        
+    }
+
+
 }
